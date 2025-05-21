ae2cdc651df5d83b3e3adf0ea5c47bdf38b4e235
KhoirZzz <khoirdev@gmail.com>
2025-05-21 12:22:55 +0700

    update: update kan dulu le

--- Generic-Type/Generic.java
+++ Generic-Type/Generic.java
@@ -2,35 +2,19 @@ import java.util.*;
 
 public class Generic {
     public static void main(String args[]) {
-        // Creating an empty Stack / Membuat Stack kosong
-        Stack<String> STACK = new Stack<String>();
-
-        // Use push() method to add elements / Menggunakan metode push() untuk menambahkan elemen
-        STACK.push("Welcome");
-        STACK.push("To");
-        STACK.push("Geeks");
-        STACK.push("For");
-        STACK.push("Geeks");
-
-        // Displaying the Stack / Menampilkan Stack
-        System.out.println("Initial Stack: " + STACK);
-
-        try {
-            // Removing elements using pop() method / Menghapus elemen menggunakan metode pop()
-            System.out.println("Popped element: " + STACK.pop());
-            System.out.println("Popped element: " + STACK.pop());
-
-            // Uncomment this if you want to test popping too much:
-            for (int i = 0; i < 10; i++) {
-                System.out.println("Popped: " + STACK.pop());
-            }
-
-        } catch (EmptyStackException e) {
-            // Catch block handles popping from an empty stack
-            System.err.println("Error: Tried to pop from an empty stack.");
-        } finally {
-            // This block always runs
-            System.out.println("Final Stack status: " + STACK);
-        }
+        Pegawai<String> pg1 = new Pegawai<String>("2428240069");
+        System.out.println("NIK: " + pg1.getNik());
     }
+}
+class Pegawai<AnyType>{
+    private AnyType nik;
+    
+    public Pegawai(AnyType nik){
+        this.nik = nik;
+    }
+
+    public AnyType getNik(){
+        return nik;
+    }
+    System.out.println("NIK: " + nik);
 }
