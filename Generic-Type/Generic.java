118e99a5acba17f3004485f8f94eda0220c948bd
KhoirZzz <khoirdev@gmail.com>
2025-05-21 12:13:59 +0700

    update: update kan dulu le

--- Generic-Type/Generic.java
+++ Generic-Type/Generic.java
@@ -1,13 +1,11 @@
-
 import java.util.*;
 
 public class Generic {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         // Creating an empty Stack / Membuat Stack kosong
         Stack<String> STACK = new Stack<String>();
 
-        // Use add() method to add elements / Menggunakan metode add() untuk menambahkan elemen
+        // Use push() method to add elements / Menggunakan metode push() untuk menambahkan elemen
         STACK.push("Welcome");
         STACK.push("To");
         STACK.push("Geeks");
@@ -17,11 +15,22 @@ public class Generic {
         // Displaying the Stack / Menampilkan Stack
         System.out.println("Initial Stack: " + STACK);
 
-        // Removing elements using pop() method / Menghapus elemen menggunakan metode pop()
-        System.out.println("Popped element: " + STACK.pop());
-        System.out.println("Popped element: " + STACK.pop());
+        try {
+            // Removing elements using pop() method / Menghapus elemen menggunakan metode pop()
+            System.out.println("Popped element: " + STACK.pop());
+            System.out.println("Popped element: " + STACK.pop());
 
-        // Displaying the Stack after pop operation / Menampilkan Stack setelah operasi pop
-        System.out.println("Stack after pop operation " + STACK);
+            // Uncomment this if you want to test popping too much:
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Popped: " + STACK.pop());
+            }
+
+        } catch (EmptyStackException e) {
+            // Catch block handles popping from an empty stack
+            System.err.println("Error: Tried to pop from an empty stack.");
+        } finally {
+            // This block always runs
+            System.out.println("Final Stack status: " + STACK);
+        }
     }
 }
